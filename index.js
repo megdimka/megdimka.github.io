7d01e9b972024efcf62cf420945d31d8f918d260
megdimka <39437446+megdimka@users.noreply.github.com>
2018-05-20 12:31:11 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ function init()
 	});
 	ael("#snap", "click", () => 
 	{
-		window.open(URL.createObjectUrl(get("video").srcObject));
+		window.open(URL.createObjectURL(get("video").srcObject));
 	});
 	navigator.mediaDevices.getUserMedia(constraints).then(stream => video.srcObject = stream).catch(handleError);
 	get("#splash").remove();
